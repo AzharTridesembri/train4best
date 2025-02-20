9b1f904346e1b24ce782abd3d140b6bd29549c51
Azhartridesemberi <azhartridesembrii@gmail.com>
2025-02-20 08:18:00 +0700

    Add Footer component

---
+++ src/components/common/Footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+    return (
+      <footer className="bg-gray-800 text-white text-center py-4">
+        <p>© 2024 Your Company. All rights reserved.</p>
+      </footer>
+    );
+  }
+  
